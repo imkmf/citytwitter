13fa10795259fe269f2b214a21b1d283fa6035e6
Kristian Freeman <kristian@kristianfreeman.com>
2018-02-03 13:50:18 -0800

    Add subsections and switch to bootstrap

--- src/citytwitter.ts
+++ src/citytwitter.ts
@@ -1,5 +1,4 @@
-import { render } from 'ejs'
-import { readFile } from 'fs'
+import { readFile, writeFile } from 'fs'
 
 import template from './template'
 
@@ -16,19 +15,43 @@ export interface Section {
   subsections: [Section];
 }
 
+export interface Meta {
+  contact_url: string;
+}
+
 export interface City {
   city: string;
   state: string;
   sections: [Section];
+  meta?: Meta;
 }
 
-// const logJson = (json: City) => {
-//   console.log(json)
-// }
+const compileHtml = (data: City): string => template(data)
 
-// Should add a function to replace renderHtml:
-// pipeline where we pre-log, validate, post-log, compile, and write
-const compileHtml = (city: City): string => render(template, city)
+const validateJson = (data: City): City => {
+  console.log(`Validated JSON for ${data.city}.`)
+  return data
+}
+
+const pipeline = (data: City): string => {
+  console.log(`Trying to compile HTML for ${data.city}.`)
+  const compiled = compileHtml(data)
+  console.log(`Successfully compiled HTML for ${data.city}.`)
+  writeToFile('dist/index.html', compiled)
+  return compiled
+}
+
+const writeToFile = (filename: string, data: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    writeFile(filename, data, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 const parseFile = (filename: string, callback: ((json: City) => string)): Promise<string> => {
   return new Promise((resolve, reject) => {
@@ -36,14 +59,14 @@ const parseFile = (filename: string, callback: ((json: City) => string)): Promis
       if (err) {
         reject(err)
       } else {
-        const obj: City = JSON.parse(data)
-        const validated = obj
+        const city: City = JSON.parse(data)
+        const validated = validateJson(city)
         resolve(callback(validated))
       }
     })
   })
 }
 
-parseFile('examples/losangeles-short.json', compileHtml)
-  .then((html: string) => console.log(html))
+parseFile('examples/losangeles-short.json', pipeline)
+  .then((_: string) => console.log('Successfully ran citytwitter. Woo!'))
   .catch(err => { throw err })
--- src/template.js
+++ src/template.js
@@ -1,15 +1,78 @@
-const template = `
-<h1><%= city %>, <%= state %></h1>
+const renderCard = account => `
+  <div class="col-md-4">
+    <div class="card mb-4 box-shadow">
+      <a href="https://twitter.com/<%= account.handle %>">
+        <img class='card-img-top' src="https://avatars.io/twitter/${account.handle}" />
+      </a>
+      <div class="card-body">
+        <h4 class='card-title'><a href="https://twitter.com/${account.handle}">${account.name || account.handle}</a></h3>
+        <p class="card-text"><%= account.notes %></p>
+        <div class="d-flex justify-content-between align-items-center">
+          <small class="text-muted">@${account.handle}</small>
+        </div>
+      </div>
+    </div>
+  </div>
+`
 
-<% sections.forEach(section => { %>
-  <h2><a href="#<%= section.permalink%>"><%= section.name %></a></h2>
-  <% section.accounts.forEach(account => { %>
-    <h3><a href="https://twitter.com/<%= account.handle %>"><%= account.name || account.handle %></a></h3>
-  <% }) %>
-  <% if (section.subsections) { %>
-    <p>Subsections not implemented yet!</p>
-  <% } %>
-<% }) %>
+const template = data => `
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>${data.city} Twitter Accounts</title>
+    <meta name="description" content="Twitter accounts for ${data.city}, ${data.state}. Powered by citytwitter.">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="/" class="navbar-brand d-flex align-items-center">
+            <strong>${data.city}, ${data.state}</strong>
+          </a>
+        </div>
+      </div>
+    </header>
+
+    <main role="main">
+      <div class="album py-2 bg-light">
+        <div class="container">
+          ${data.sections.map(section => `
+            <div class="row px-4 py-2">
+              <h3><a class='link black dim' href="#${section.permalink}">${section.name}</a></h3>
+            </div>
+            <div class="row">
+              ${section.accounts.map(account => renderCard(account))}
+            </div>
+            ${section.subsections.map(subsection => {
+              <div class="row px-4 py-2">
+                <h4><a class='link black dim' href="#${subsection.permalink}">${subsection.name}</a></h4>
+              </div>
+              <div class="row">
+                ${subsection.accounts.map(account => renderCard(account))}
+              </div>
+            })}
+          `}
+        </div>
+      </div>
+    </main>
+
+    <footer class="text-muted">
+      <div class="container py-2">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>This site was built with <a href="https://github.com/imkmf/citytwitter">citytwitter</a>, a tool for visualizing social media accounts of United States cities.</p>
+        ${if (data.meta) {
+          <p>Something out of date? <a href="<%= meta.contact_url %>">Contact the maintainer</a>.</p>
+        }}
+      </div>
+    </footer>
+  </body>
+</html>
 `
 
 export default template
